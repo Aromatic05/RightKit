4597059ad56897a1a4ae722790f2ac4f0c404646
Aromatic <symwww@outlook.com>
2025-08-15 17:40:55 +0800

    Initial Commit

---
+++ RightKit/RightKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  RightKitApp.swift
+//  RightKit
+//
+//  Created by Yiming Sun on 2025/8/15.
+//
+
+import SwiftUI
+
+@main
+struct RightKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
