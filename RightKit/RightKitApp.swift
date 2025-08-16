226faba2fe9f610d343f1b52a343166652651112
Aromatic <symwww@outlook.com>
2025-08-16 14:17:00 +0800

    feat: permission

--- RightKit/RightKitApp.swift
+++ RightKit/RightKitApp.swift
@@ -13,6 +13,9 @@ struct RightKitApp: App {
     init() {
         // 在应用启动时初始化默认配置
         ConfigurationManager.initializeDefaultConfiguration()
+        
+        // 检查并请求“完全磁盘访问权限”
+        PermissionManager.checkAndRequestFullDiskAccess()
     }
     
     var body: some Scene {
